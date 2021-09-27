05a3013378618c1b688225c6882e5e992a309a1b
boyan <17426470+boyan01@users.noreply.github.com>
2021-09-27 13:50:12 +0800

    add win_toast plugin

---
+++ packages/win_toast/CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.0.1 21/9/27
+
+* add basic functions.
---
+++ packages/win_toast/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <win_toast/win_toast_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WinToastPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WinToastPlugin"));
+}
