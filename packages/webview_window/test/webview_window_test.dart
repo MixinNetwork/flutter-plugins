1a0cb22b7517b8bc592f31104c4f86221e55ea57
boyan <17426470+boyan01@users.noreply.github.com>
2021-10-19 15:28:55 +0800

    remove test

--- packages/webview_window/test/webview_window_test.dart
+++ packages/webview_window/test/webview_window_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:webview_window/webview_window.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('webview_window');
@@ -15,9 +14,5 @@ void main() {
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
-  });
-
-  test('getPlatformVersion', () async {
-    expect(await WebviewWindow.platformVersion, '42');
   });
 }
