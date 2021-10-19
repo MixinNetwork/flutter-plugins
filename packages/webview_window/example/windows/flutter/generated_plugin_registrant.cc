5e4cf5fa2558d1e43584490bd42e1c868cf519b0
boyan <17426470+boyan01@users.noreply.github.com>
2021-10-19 15:32:23 +0800

    add Windows files

---
+++ packages/webview_window/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <webview_window/webview_window_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WebviewWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WebviewWindowPlugin"));
+}
