7c34326cea60bfa4b3909ff797d18d8ff0814df4
boyan <17426470+boyan01@users.noreply.github.com>
2021-12-07 17:02:00 +0800

    remove macOS plugin unused method

--- packages/fts5_simple/macos/Classes/Fts5SimplePlugin.swift
+++ packages/fts5_simple/macos/Classes/Fts5SimplePlugin.swift
@@ -3,17 +3,6 @@ import FlutterMacOS
 
 public class Fts5SimplePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "fts5_simple", binaryMessenger: registrar.messenger)
-    let instance = Fts5SimplePlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("macOS " + ProcessInfo.processInfo.operatingSystemVersionString)
-    default:
-      result(FlutterMethodNotImplemented)
-    }
   }
 }
