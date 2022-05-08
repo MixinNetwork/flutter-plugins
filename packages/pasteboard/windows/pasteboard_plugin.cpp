377b7ea0f164c92bd5313a469750061f85fbe1aa
alexkmbk <alexkmbk@gmail.com>
2022-05-08 22:54:58 +0600

    49381 clipboard code support for HTML format

--- packages/pasteboard/windows/pasteboard_plugin.cpp
+++ packages/pasteboard/windows/pasteboard_plugin.cpp
@@ -429,15 +429,25 @@ void PasteboardPlugin::HandleMethodCall(
       return;
     }
     SetClipboardData(CF_HDROP, storage.hGlobal);
-    result->Success();
-  } else if (method_call.method_name() == "html"){
-    
-    #define CF_HTML 49380
+	result->Success();
+  }
+  else if (method_call.method_name() == "html") {
 
-    if (!IsClipboardFormatAvailable(CF_HTML)) {
-      result->Success();
-      return;
-    }
+  UINT CF_HTML = 49380;
+  bool isHTMLFormatAvailable = false;
+
+  if (IsClipboardFormatAvailable(CF_HTML)) {
+	  isHTMLFormatAvailable = true;
+  }
+  else if (IsClipboardFormatAvailable(49381)) {
+	  isHTMLFormatAvailable = true;
+	  CF_HTML = 49381;
+  }
+
+  if (!isHTMLFormatAvailable) {
+	  result->Success();
+	  return;
+  }
 
 	if (!OpenClipboard(nullptr)) {
 		result->Error("0", "open clipboard failed");
