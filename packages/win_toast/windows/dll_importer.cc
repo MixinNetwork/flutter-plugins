5f07b62b518d1de1689bf490d66dab12d7d02c9d
boyan01 <17426470+boyan01@users.noreply.github.com>
2022-12-08 17:51:26 +0800

    remove unnecessary files

--- packages/win_toast/windows/dll_importer.cc
+++ packages/win_toast/windows/dll_importer.cc
@@ -22,43 +22,15 @@ HRESULT loadFunctionFromLibrary(HINSTANCE library, LPCSTR name, Function &func) 
 HRESULT LoadFunctions() {
   HINSTANCE LibKernel32 = LoadLibraryW(L"KERNEL32.DLL");
 
-  RETURN_IF_FAILED(loadFunctionFromLibrary(LibKernel32, "GetCurrentPackageFullName",
-                                           DllImporter::GetCurrentPackageFullName));
   RETURN_IF_FAILED(loadFunctionFromLibrary(LibKernel32, "GetPackageFamilyName",
                                            DllImporter::GetPackageFamilyName));
 
-  HINSTANCE LibShell32 = LoadLibraryW(L"SHELL32.DLL");
-  RETURN_IF_FAILED(loadFunctionFromLibrary(LibShell32, "SetCurrentProcessExplicitAppUserModelID",
-                                           DllImporter::SetCurrentProcessExplicitAppUserModelID));
-
-  HINSTANCE LibPropSys = LoadLibraryW(L"PROPSYS.DLL");
-  RETURN_IF_FAILED(loadFunctionFromLibrary(LibPropSys, "PropVariantToString",
-                                           DllImporter::PropVariantToString));
-
-  HINSTANCE LibComBase = LoadLibraryW(L"COMBASE.DLL");
-  RETURN_IF_FAILED(loadFunctionFromLibrary(LibComBase, "RoGetActivationFactory",
-                                           DllImporter::RoGetActivationFactory));
-
-  RETURN_IF_FAILED(loadFunctionFromLibrary(LibComBase, "WindowsCreateStringReference",
-                                           DllImporter::WindowsCreateStringReference));
-  RETURN_IF_FAILED(loadFunctionFromLibrary(LibComBase, "WindowsGetStringRawBuffer",
-                                           DllImporter::WindowsGetStringRawBuffer));
-  RETURN_IF_FAILED(loadFunctionFromLibrary(LibComBase, "WindowsDeleteString",
-                                           DllImporter::WindowsDeleteString));
   return S_OK;
 }
 
 }
 
-DllImporter::f_RoGetActivationFactory DllImporter::RoGetActivationFactory;
-DllImporter::f_WindowsCreateStringReference DllImporter::WindowsCreateStringReference;
-DllImporter::f_WindowsGetStringRawBuffer DllImporter::WindowsGetStringRawBuffer;
-DllImporter::f_WindowsDeleteString DllImporter::WindowsDeleteString;
-DllImporter::f_GetCurrentPackageFullName DllImporter::GetCurrentPackageFullName;
-DllImporter::f_SetCurrentProcessExplicitAppUserModelID DllImporter::SetCurrentProcessExplicitAppUserModelID;
-DllImporter::f_PropVariantToString DllImporter::PropVariantToString;
 DllImporter::f_GetPackageFamilyName DllImporter::GetPackageFamilyName;
-
 
 HRESULT DllImporter::Initialize() {
   if (is_dll_load_called) {
--- packages/win_toast/windows/dll_importer.h
+++ packages/win_toast/windows/dll_importer.h
@@ -14,38 +14,13 @@ class DllImporter {
 
  public:
 
-  typedef HRESULT(FAR STDAPICALLTYPE *f_SetCurrentProcessExplicitAppUserModelID)(__in PCWSTR AppID);
-  typedef HRESULT
-  (FAR STDAPICALLTYPE *f_PropVariantToString)(_In_ REFPROPVARIANT propvar, _Out_writes_(cch) PWSTR psz, _In_ UINT cch);
-  typedef HRESULT(FAR STDAPICALLTYPE *f_RoGetActivationFactory)
-      (_In_ HSTRING activatableClassId, _In_ REFIID iid, _COM_Outptr_ void **factory);
-  typedef HRESULT(FAR STDAPICALLTYPE *f_WindowsCreateStringReference)(_In_reads_opt_(length + 1)
-                                                                      PCWSTR sourceString,
-                                                                      UINT32 length,
-                                                                      _Out_
-                                                                      HSTRING_HEADER *hstringHeader,
-                                                                      _Outptr_result_maybenull_
-                                                                      _Result_nullonfailure_
-                                                                      HSTRING *string);
-  typedef PCWSTR(FAR STDAPICALLTYPE *f_WindowsGetStringRawBuffer)(_In_ HSTRING string, _Out_opt_ UINT32 *length);
-  typedef HRESULT(FAR STDAPICALLTYPE *f_WindowsDeleteString)(_In_opt_ HSTRING string);
-
-  typedef HRESULT(FAR STDAPICALLTYPE *f_GetCurrentPackageFullName)
-      (_Inout_ UINT32 *packageFullNameLength, _Out_writes_opt_(*packageFullNameLength) PWSTR packageFullName);
-
   typedef HRESULT(FAR STDAPICALLTYPE *f_GetPackageFamilyName)
       (_In_ HANDLE hProcess,
        _Inout_ UINT32 *packageFamilyNameLength,
        _Out_writes_opt_(*packageFamilyNameLength) PWSTR packageFamilyName
       );
 
-  static f_GetCurrentPackageFullName GetCurrentPackageFullName;
-  static f_SetCurrentProcessExplicitAppUserModelID SetCurrentProcessExplicitAppUserModelID;
-  static f_PropVariantToString PropVariantToString;
-  static f_RoGetActivationFactory RoGetActivationFactory;
-  static f_WindowsCreateStringReference WindowsCreateStringReference;
-  static f_WindowsGetStringRawBuffer WindowsGetStringRawBuffer;
-  static f_WindowsDeleteString WindowsDeleteString;
+
   static f_GetPackageFamilyName GetPackageFamilyName;
 
   static HRESULT Initialize();
