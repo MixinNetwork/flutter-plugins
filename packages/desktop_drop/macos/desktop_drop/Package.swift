cf46b7eee6050689e7aed29337b8c6e300e1ac7e
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2025-04-15 16:52:14 +0200

    macos migration

---
+++ packages/desktop_drop/macos/desktop_drop/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "desktop_drop",
+    platforms: [
+        .macOS("10.11")
+    ],
+    products: [
+        .library(name: "desktop-drop", targets: ["desktop_drop"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "desktop_drop",
+            dependencies: [],
+            resources: [
+                .process("PrivacyInfo.xcprivacy"),
+            ]
+        )
+    ]
+)
