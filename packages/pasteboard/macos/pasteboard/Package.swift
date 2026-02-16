0da82d63ad17726aa609d89496398dcec8852418
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2026-02-16 11:07:43 +0100

    feat: Migrate macOS example project from CocoaPods to Swift Package Manager, update Xcode scheme, and app delegate.

---
+++ packages/pasteboard/macos/pasteboard/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "pasteboard",
+    platforms: [
+        .macOS("10.11")
+    ],
+    products: [
+        .library(name: "pasteboard", targets: ["pasteboard"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "pasteboard",
+            dependencies: [],
+            resources: []
+        )
+    ]
+)
